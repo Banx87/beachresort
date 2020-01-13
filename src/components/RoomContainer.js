aeacea8d2d7297d9d5afcc56000559bf548ed3f1
Daemon87 <joni.lindholm@gmail.com>
2020-01-13 15:37:08 +0200

    Added RoomContainer, RoomFilter and RoomList

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import Loading from "./Loading";
+import { withRoomConsumer } from "../Context";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      Hello From RoomsContainer
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </div>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomFilter from "./RoomFilter";
+// import RoomList from "./RoomList";
+// import { RoomConsumer } from "../Context";
+// import Loading from "./Loading";
+
+// export default function RoomsContainer() {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello From RoomsContainer
+//             <RoomFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
